6e333c783da71c4f94e562e9b1fb1878feec7bba
weslley <dev.wfeduardo@gmail.com>
2024-01-24 15:19:46 -0300

    adicionando toast

--- src/app/auth/components/form.tsx
+++ src/app/auth/components/form.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 const signInForm = z.object({
@@ -20,17 +21,20 @@ export function Form() {
   } = useForm<SignInFormSchema>()
 
   function handleSignIn(data: SignInFormSchema) {
+    toast.success('Um link de login foi enviado para seu email.')
     console.log(data)
   }
 
-  return <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
-  <div className="space-y-2">
-    <Label htmlFor="email">Seu e-mail</Label>
-    <Input id="email" type="email" {...register('email')} />
-  </div>
+  return (
+    <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+      <div className="space-y-2">
+        <Label htmlFor="email">Seu e-mail</Label>
+        <Input id="email" type="email" {...register('email')} />
+      </div>
 
-  <Button disabled={isSubmitting} type="submit" className="w-full">
-    {isSubmitting ? 'Acessando...' : 'Acessar painel'}
-  </Button>
-</form>
+      <Button disabled={isSubmitting} type="submit" className="w-full">
+        {isSubmitting ? 'Acessando...' : 'Acessar painel'}
+      </Button>
+    </form>
+  )
 }
