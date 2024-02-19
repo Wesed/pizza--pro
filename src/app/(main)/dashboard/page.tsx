f2cc188bfd52ef586de265ef2d055d12edcc5a6a
weslley <dev.wfeduardo@gmail.com>
2024-02-19 16:04:41 -0300

    adiccionando cards da dashboard

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,8 +1,24 @@
 import { Metadata } from 'next'
+import { MonthRevenueCard } from './month-revenue-card'
+import { MonthOrdersAmountCard } from './mont-orders-amount-card'
+import { DayOrderAmountCard } from './day-orders-amount-card'
+import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
 }
 export default function Dashboard() {
-  return <h1 className=""> dashboard </h1>
+  return (
+    <div className="">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrderAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+      </div>
+    </div>
+  )
 }
