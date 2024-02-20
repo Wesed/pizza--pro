7da3e413c3f64314faf4a3a1a69e4ea55bb9476d
weslley <dev.wfeduardo@gmail.com>
2024-02-20 18:16:30 -0300

    add component - popular-products-chart

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { MonthRevenueCard } from './month-revenue-card'
 import { MonthOrdersAmountCard } from './mont-orders-amount-card'
 import { DayOrderAmountCard } from './day-orders-amount-card'
 import { MonthCanceledOrdersAmountCard } from './month-canceled-orders-amount-card'
+import { RevenueChart } from './revenue-chart'
+import { PopularProductsChart } from './popular-products-chart'
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -17,6 +19,11 @@ export default function Dashboard() {
           <MonthOrdersAmountCard />
           <DayOrderAmountCard />
           <MonthCanceledOrdersAmountCard />
+        </div>
+
+        <div className="grid grid-cols-10 gap-4">
+          <RevenueChart />
+          <PopularProductsChart />
         </div>
       </div>
     </div>
---
+++ src/app/(main)/dashboard/popular-products-chart.tsx
@@ -0,0 +1,89 @@
+'use client'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+const data = [
+  { product: '4 Queijos', amount: 50 },
+  { product: 'Portuguesa', amount: 42 },
+  { product: 'Mussarela', amount: 38 },
+  { product: 'Frango', amount: 26 },
+  { product: 'Marguerita', amount: 16 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+]
+
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-4">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-semibold">
+            Produtos populares
+          </CardTitle>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart data={data} style={{ fontSize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={({
+                cx,
+                cy,
+                midAngle,
+                innerRadius,
+                outerRadius,
+                value,
+                index,
+              }) => {
+                const RADIAN = Math.PI / 180
+                const radius = 12 + innerRadius + (outerRadius - innerRadius)
+                const x = cx + radius * Math.cos(-midAngle * RADIAN)
+                const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+                return (
+                  <text
+                    x={x}
+                    y={y}
+                    className="fill-muted-foreground text-xs"
+                    textAnchor={x > cx ? 'start' : 'end'}
+                    dominantBaseline="central"
+                  >
+                    {data[index].product.length > 12
+                      ? data[index].product.substring(0, 12).concat('...')
+                      : data[index].product}{' '}
+                    ({value})
+                  </text>
+                )
+              }}
+            >
+              {data.map((_, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index]}
+                  className="stroke-background hover:opacity-80"
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
