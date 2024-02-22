36754c016ecca062e5adb5cbe9f8dc333a5c3a52
weslley <dev.wfeduardo@gmail.com>
2024-02-21 23:35:14 -0300

    adicionando react-query ao projeto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from 'next/font/google'
 import './globals.css'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/components/theme/theme-provider'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 
 const roboto = Roboto({
   weight: '400',
@@ -26,7 +28,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <ThemeProvider attribute="class" defaultTheme="system">
-          {children}
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
         </ThemeProvider>
         <Toaster richColors />
       </body>
