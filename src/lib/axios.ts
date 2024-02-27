a6a8288ce72fac4f00fed5d9bcf37d496947f95e
weslley <dev.wfeduardo@gmail.com>
2024-02-26 22:54:22 -0300

    adicionando uma feature ao axios.ts de delay nas requisicoes pra testar skeleton

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -13,3 +13,10 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 })
+
+if (process.env.NEXT_PUBLIC_ENABLE_API_DELAY === 'true') {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    return config
+  })
+}
