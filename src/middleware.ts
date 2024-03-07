12f9cabc19df5b4488feec217546d63b4e8d1da5
weslley <dev.wfeduardo@gmail.com>
2024-03-07 16:02:16 -0300

    implementando middlware - redireciona pra /sign-in quando nao tem token

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { api } from './lib/axios'
+import { isAxiosError } from 'axios'
+
+export async function middleware(request: NextRequest) {
+  console.log('ponto')
+  const token = request.cookies.get('auth')
+  if (!token) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
+  return NextResponse.next()
+
+  // const interceptorId = api.interceptors.response.use(
+  //   (response) => response,
+  //   async (error) => {
+  //     if (isAxiosError(error)) {
+  //       console.log('aq')
+  //       const status = error.response?.status
+  //       const code = error.response?.data.code
+
+  //       if (status === 401 && code === 'Unauthorized') {
+  //         console.log('aq', status, code)
+  //         api.interceptors.response.eject(interceptorId)
+  //         return NextResponse.redirect(new URL('/sign-in', request.url))
+  //       }
+  //     }
+
+  //     // Se não for um erro de autenticação, continue com a solicitação
+  //     api.interceptors.response.eject(interceptorId)
+  //     return NextResponse.next()
+  //   },
+  // )
+
+  // Se não houver erro, continue com a solicitação
+}
+
+export const config = {
+  matcher: ['/dashboard', '/orders'],
+}
